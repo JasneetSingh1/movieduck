bf385493de6c2a07a766d7c166cda3829f4d2d74
JasneetSingh1 <jasneetpalsingh619@gmail.com>
2024-08-16 16:44:49 -0600

    added state to moviesgrid

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import '../styles.css';
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <div className="header">
-            <img className="logo" src="logo.png" alt="movieduck"/>
-            <h2 className="app-subtitle">It's time for your popcorn! Fid your next movie here.</h2>
+            <img className="logo" src="logo.png" alt="movieduck" />
+            <h2 className="app-subtitle">It's time for your popcorn! Find your next movie here.</h2>
 
         </div>
     );
