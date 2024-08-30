853c5606b3877f938b05e8d240785045cec21b27
Jasneet <jasneetpalsingh619@gmail.com>
2024-08-30 07:51:19 -0600

    added toggle ui

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,11 +1,20 @@
 import React from "react";
 import '../styles.css';
+import MovieCard from "./MovieCard";
 
-export default function Watchlist({movies, Watchlist, toggleWatchlist}) {
+export default function Watchlist({movies, watchlist, toggleWatchlist}) {
     return (
 
         <div>
             <h1 className="title">Watchlist</h1>
+            <div className="watchlist">
+                {
+                    watchlist.map(id => {
+                        const movie = movies.find(movie => movie.id === id)
+                        return <MovieCard key={id} movie={movie} toggleWatchlist={toggleWatchlist} isWatchlisted={true}></MovieCard>
+                    })
+                }
+            </div>
         </div>
     )
 }
